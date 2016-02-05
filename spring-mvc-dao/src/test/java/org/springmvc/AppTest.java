d4d3ddf3fdbe672ba72439f4be80224586a03f51
Eman Nollase <exst_enollase@yahoo.com>
2016-02-05 14:24:46 +0800

    added spring junit

--- spring-mvc-dao/src/test/java/org/springmvc/AppTest.java
+++ spring-mvc-dao/src/test/java/org/springmvc/AppTest.java
@@ -1,38 +1,53 @@
 package org.springmvc;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springmvc.dao.person.PersonDao;
+import org.springmvc.model.Address;
+import org.springmvc.model.Person;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/application-jpa-context-test.xml","/app-jpa-dao-ctx-test.xml"})
+public class AppTest extends AbstractJUnit4SpringContextTests
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    private PersonDao personDao;
+
+    @Autowired
+    public void setPersonDao(PersonDao personDao) {
+        this.personDao = personDao;
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    @Rollback(false)
+    public void testSavePerson(){
+        Person person = new Person();
+        Address address = new Address();
+        person.setFirstName("Eman");
+        person.setLastName("Nollase");
+        person.setMiddleInitial('A');
+        address.setCity("Pasig");
+        address.setCity("Pasig");
+        address.setStreetAddress1("#6 2nd St.");
+        address.setStreetAddress2("Kapitolyo");
+        person.setAddress(address);
+        personDao.save(person);
+        System.out.println("person id: => "+person.getId());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testList(){
+        System.out.println(personDao.loadAll());
     }
 }
